3348631b0e8f4a8fe9122ff9cca54a9053d10c07
Ben Chatelain <ben@octop.ad>
2017-11-20 14:57:59 -0700

    Wire up button to text view

--- app/src/main/java/at/phatbl/androidmasterclass/MainActivity.kt
+++ app/src/main/java/at/phatbl/androidmasterclass/MainActivity.kt
@@ -2,6 +2,8 @@ package at.phatbl.androidmasterclass
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
 /**
  * Main activity.
@@ -10,5 +12,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun buttonClicked(view: View) {
+        val textView = findViewById<TextView>(R.id.message)
+        textView.text = "Button clicked"
     }
 }
