b6af0e19252e87fa8bebd1c3172534e930ae07ab
Ben Chatelain <ben.d.chatelain@kp.org>
2017-11-21 15:43:30 -0700

    Add FoodListActivity

---
+++ app/src/main/java/at/phatbl/androidmasterclass/FoodListActivity.kt
@@ -0,0 +1,11 @@
+package at.phatbl.androidmasterclass
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class FoodListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_food_list)
+    }
+}
